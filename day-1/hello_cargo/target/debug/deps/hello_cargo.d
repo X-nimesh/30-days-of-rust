138cb7fd74fbecb692ad1ac777679575cae7a41f
X.Nimesh <X.nimesh7@gmail.com>
2024-05-15 17:30:00 +0545

    Day-2

--- day-1/hello_cargo/target/debug/deps/hello_cargo.d
+++
@@ -1,5 +0,0 @@
-\\wsl.localhost\Ubuntu\home\nimesh\programming\30-days-of-rust\hello_cargo\target\debug\deps\hello_cargo.exe: src\main.rs
-
-\\wsl.localhost\Ubuntu\home\nimesh\programming\30-days-of-rust\hello_cargo\target\debug\deps\hello_cargo.d: src\main.rs
-
-src\main.rs:
